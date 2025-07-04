768053f4c15b0494477207260adabcaf76b30801
Muhammad Murtaza Hassan <136434291+murtz544@users.noreply.github.com>
2025-07-04 17:29:46 +0500

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,3 @@ username_batches = [df['Usernames'].tolist()[i:i + 30] for i in range(0, len(df)
 
 print("Total usernames:", len(df))
 print("Total batches:", len(username_batches))
-for i, batch in enumerate(username_batches):
-    print(f"Batch {i + 1}: {len(batch)} usernames")
-    print(batch)
-    print()  # Print a newline for better readability
